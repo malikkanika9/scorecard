78d895138ef2c1c5e2a0c571ab6dac8a5bb3c077
kanika <malikkanika9@gmail.com>
2022-09-08 16:47:28 +0530

    table

--- src/components/Tablee/Tablee.jsx
+++ src/components/Tablee/Tablee.jsx
@@ -2,35 +2,41 @@ import React from "react";
 import {Col,Container, Row,} from "react-bootstrap";
 import {Marks} from "../Marks/Marks";
 import {Total} from "../Total/Total";
+import Part2 from "./Part2";
 export const Tablee = () => {
   return (
     <>
-      <Container  className="border primary mt-4">
+      <Container className="border primary mt-4">
         <Row className="mt-2 ">
-          <Col className="text-center border " > 
-            <h4 style={{color:"red"}}> First Terminal Examination 2018-2019</h4>
+          <Col className=" text-center border">
+            <h4>First Examination 29</h4>
           </Col>
         </Row>
         <Row className="mt-2">
           <Col className=" text-center border">
-            <h5 style={{font: "bold"}}>Academic Performance</h5>
+            <h5>First Terminal Examination 2018-2019</h5>
           </Col>
         </Row>
         <Row className="mt-2">
           <Col className=" text-center border">
-            <h5>First Examination 29</h5>
+            <h5>Academic Performance</h5>
           </Col>
         </Row>
+
         <Row className="mt-2 border">
-          <Col className=" text-center border" xs lg="6">
+          <Col className=" text-center border" xs lg="7">
             <Row className="border">
               <h5 style={{ color: "blue" }}>Part-I -:Scholastic Areas</h5>
             </Row>
             <Row className="mt-1">
               <Col className=" text-center border fw-bold p-2" xs lg="1">
-                Sr. No
+                Sr.No
               </Col>
-              <Col className=" text-center border fw-bold text-black p-2"xs lg="2" >
+              <Col
+                className=" text-center border fw-bold text-Black"
+                xs
+                lg="2"
+              >
                 Subjects
               </Col>
               <Col className=" text-center border" xs lg="9">
@@ -43,14 +49,16 @@ export const Tablee = () => {
                     Total
                   </Col>
                 </Row>
-{/*  out of marks */}
-   <Total/>
+                <Total />
               </Col>
             </Row>
             <Marks/>
           </Col>
-          <Col className=" text-center border">
-            <h5 style={{ color: "blue" }} >Part-II :-Co-Scholastic Areas</h5>
+          <Col className="  border-primary      ">
+            <Row className="border text-center">
+              <h5 style={{ color: "blue" }}>Part-II :-Co-Scholastic Areas</h5>
+            </Row>
+         <Part2/>
           </Col>
         </Row>
       </Container>
